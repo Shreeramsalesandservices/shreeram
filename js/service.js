86d53cd1a77ffbb2b81b5a9dc7d832c73180adfa
Shreeramsalesandservices <shreeramsalesandservice894@gmail.com>
2026-08-05 21:49:48 +0530

    Delete js directory

--- js/service.js
+++
@@ -1,9 +0,0 @@
-  function toggleDropdown(button) {
-    const dropdownContent = button.nextElementSibling;
-
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-}
